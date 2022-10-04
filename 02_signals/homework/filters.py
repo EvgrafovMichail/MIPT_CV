38bf06d61eac1783e649ecc4a6d31abb55f0a263
EvgrafovMichal <autumnevilmen@gmail.com>
2022-10-05 01:32:39 +0300

    half of hw2 is done

--- 02_signals/homework/filters.py
+++ 02_signals/homework/filters.py
@@ -1,3 +1,4 @@
+from numpy.lib.stride_tricks import as_strided
 import numpy as np
 
 
@@ -9,21 +10,35 @@ def conv_nested(image, kernel):
     the result that has the same shape as the input image.
 
     Args:
-        image: numpy array of shape (Hi, Wi).
-        kernel: numpy array of shape (Hk, Wk).
+        image: numpy array of shape (hi, wi).
+        kernel: numpy array of shape (hk, wk).
 
     Returns:
-        out: numpy array of shape (Hi, Wi).
+        out: numpy array of shape (hi, wi).
     """
-    Hi, Wi = image.shape
-    Hk, Wk = kernel.shape
-    out = np.zeros((Hi, Wi))
+    hi, wi = image.shape
+    hk, wk = kernel.shape
 
-    ### YOUR CODE HERE
-    pass
-    ### END YOUR CODE
+    out = np.zeros((hi, wi))
+
+    for i in range(hi):
+        for j in range(wi):
+            for k in range(hk):
+                for m in range(wk):
+
+                    image_i, image_j = i + k - hk // 2, j + m - wk // 2
+
+                    if image_i < 0 or image_i >= hi:
+                        continue
+
+                    if image_j < 0 or image_j >= wi:
+                        continue
+
+                    value = image[image_i, image_j] * kernel[hk - k - 1, wk - m - 1]
+                    out[i, j] += value
 
     return out
+
 
 def zero_pad(image, pad_height, pad_width):
     """ Zero-pad an image.
@@ -35,20 +50,19 @@ def zero_pad(image, pad_height, pad_width):
          [0, 0, 0, 0, 0]]         of shape (3, 5)
 
     Args:
-        image: numpy array of shape (H, W).
+        image: numpy array of shape (h, w).
         pad_width: width of the zero padding (left and right padding).
         pad_height: height of the zero padding (bottom and top padding).
 
     Returns:
-        out: numpy array of shape (H+2*pad_height, W+2*pad_width).
+        out: numpy array of shape (h+2*pad_height, w+2*pad_width).
     """
 
-    H, W = image.shape
-    out = np.zeros_like(image)
+    h, w = image.shape
 
-    ### YOUR CODE HERE
-    pass
-    ### END YOUR CODE
+    out = np.zeros((h + 2 * pad_height, w + 2 * pad_width))
+    out[pad_height: h + pad_height, pad_width: w + pad_width] = image
+
     return out
 
 
@@ -65,40 +79,51 @@ def conv_fast(image, kernel):
         - You may find np.flip() and np.sum() useful
 
     Args:
-        image: numpy array of shape (Hi, Wi).
-        kernel: numpy array of shape (Hk, Wk).
+        image: numpy array of shape (hi, wi).
+        kernel: numpy array of shape (hk, wk).
 
     Returns:
-        out: numpy array of shape (Hi, Wi).
+        out: numpy array of shape (hi, wi).
     """
-    Hi, Wi = image.shape
-    Hk, Wk = kernel.shape
-    out = np.zeros((Hi, Wi))
+    hi, wi = image.shape
+    hk, wk = kernel.shape
 
-    ### YOUR CODE HERE
-    pass
-    ### END YOUR CODE
+    out = np.zeros((hi, wi))
+    ker_reversed = np.flip(kernel)
+    padded = zero_pad(image, hk // 2, wk // 2)
+
+    for i in range(hk // 2, hk // 2 + hi):
+        for j in range(wk // 2, wk // 2 + wi):
+
+            val_conv = padded[i - hk // 2: i + hk // 2 + 1, j - wk // 2: j + wk // 2 + 1]
+            out[i - hk // 2, j - wk // 2] = np.sum(val_conv * ker_reversed)
 
     return out
+
 
 def conv_faster(image, kernel):
     """
     Args:
-        image: numpy array of shape (Hi, Wi).
-        kernel: numpy array of shape (Hk, Wk).
+        image: numpy array of shape (hi, wi).
+        kernel: numpy array of shape (hk, wk).
 
     Returns:
-        out: numpy array of shape (Hi, Wi).
+        out: numpy array of shape (hi, wi).
     """
-    Hi, Wi = image.shape
-    Hk, Wk = kernel.shape
-    out = np.zeros((Hi, Wi))
 
-    ### YOUR CODE HERE
-    pass
-    ### END YOUR CODE
+    hi, wi = image.shape
+    hk, wk = kernel.shape
+
+    ker_reversed = np.flip(kernel)
+    padded = zero_pad(image, hk // 2, wk // 2)
+
+    strided_shape = (hi, wi, hk, wk)
+    padded_strided = as_strided(padded, strided_shape, strides=padded.strides * 2)
+
+    out = np.sum(padded_strided * ker_reversed, axis=(2, 3))
 
     return out
+
 
 def cross_correlation(f, g):
     """ Cross-correlation of f and g.
@@ -119,6 +144,7 @@ def cross_correlation(f, g):
     ### END YOUR CODE
 
     return out
+
 
 def zero_mean_cross_correlation(f, g):
     """ Zero-mean cross-correlation of f and g.
@@ -141,6 +167,7 @@ def zero_mean_cross_correlation(f, g):
     ### END YOUR CODE
 
     return out
+
 
 def normalized_cross_correlation(f, g):
     """ Normalized cross-correlation of f and g.
